736c332eb7caaef5ba81823be7768b64d0f79d4d
Djalma <djalma.ant@gmail.com>
2020-05-12 21:53:15 -0300

    CRUD Scaffolding

---
+++ GigaWebMvc/Data/GigaWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GigaWebMvc.Models;
+
+namespace GigaWebMvc.Data
+{
+    public class GigaWebMvcContext : DbContext
+    {
+        public GigaWebMvcContext (DbContextOptions<GigaWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GigaWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- GigaWebMvc/Startup.cs
+++ GigaWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GigaWebMvc.Data;
 
 namespace GigaWebMvc
 {
@@ -33,6 +35,9 @@ namespace GigaWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<GigaWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("GigaWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
