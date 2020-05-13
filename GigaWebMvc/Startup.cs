0df9f3257cc708d488b41295dfb45fa42aecf4e1
Djalma <djalma.ant@gmail.com>
2020-05-13 19:54:43 -0300

    MySQL adaptation and first migration

--- GigaWebMvc/Startup.cs
+++ GigaWebMvc/Startup.cs
@@ -37,7 +37,8 @@ namespace GigaWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<GigaWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("GigaWebMvcContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("GigaWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("GigaWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
