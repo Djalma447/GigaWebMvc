aa273d2a6ac325ac8d350effbc7df2613055cc3b
Djalma <djalma.ant@gmail.com>
2020-09-22 23:09:37 -0300

    Implementing simple search

---
+++ GigaWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using GigaWebMvc.Data;
+using GigaWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigaWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly GigaWebMvcContext _context;
+
+        public SalesRecordService(GigaWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
